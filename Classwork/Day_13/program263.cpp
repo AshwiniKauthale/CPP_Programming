7f64230559c4b89682075e839888f76dff880873
AshwiniKauthale <kauthaleashwini40@gmail.com>
2025-12-01 00:15:58 +0530

    Add files via upload

---
+++ Classwork/Day_13/program263.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+template<class T>
+
+T Add(T No1,T No2)
+    {
+        T Ans;
+        Ans = No1 + No2;
+        return Ans;
+    }
+
+int main()
+{
+    int iValue1 = 10, iValue2 = 11,iRet = 0;
+    float fValue1 = 10, fValue2 = 11,fRet = 0;
+    double dValue1 = 10, dValue2 = 11,dRet = 0;
+
+    iRet = Add(iValue1,iValue2);
+    cout<<"Addition of int :"<<iRet<<"\n";
+
+    fRet = Add(fValue1,fValue2);
+    cout<<"Addition of int :"<<fRet<<"\n";
+
+    dRet = Add(dValue1,dValue2);
+    cout<<"Addition of int :"<<dRet<<"\n";
+
+    return 0;
+}
